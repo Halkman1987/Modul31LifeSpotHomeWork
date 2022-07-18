b4174bfc5923b7de5147195de0a807398b8b7636
halkman <halk-man@mail.ru>
2022-07-18 18:28:56 +0500

    Добавьте файлы проекта.

---
+++ LifeSpot/Static/JS/about.js
@@ -0,0 +1,52 @@
+﻿/*
+* Запросим пользовательский ввод
+* и сохраним отзыв в объект
+* 
+* */
+function getComment() {
+    let comment = {}
+    comment.autor = prompt("What is you name?")
+    if (comment.autor == nul) { return }
+    commemt.text = prompt("Enter calback")
+    if (comment.text == null) { return }
+    comment.date = new Date().toLocaleString()
+    let Likes = prompt("Wont like comment")
+    if (Likes) {
+        let 
+    }
+}
+
+
+function getReview() {
+    // Создадим объект
+    let review = {}
+    
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if(review["userName"] == null){
+        return
+    }
+    
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if(review["comment"] == null){
+        return
+    }
+    
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+    
+    // Добавим на страницу
+    writeReview(review)
+}
+
+/*
+* Запишем отзыв на страницу 
+* 
+* */
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>`  +
+        '</div>';
+}
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,47 @@
+﻿let nsession = {
+    'startDate': new Date().toLocaleString(),
+    'userAgent': window.navigator.userAgent,
+    'userAge': prompt("Пожалуйста, введите ваш возраст?")
+}
+
+function NcheckAge() {
+    if (nsession.userAge >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+let NsessionLog = function () {
+    console.log('Начало сессии: ' + nsession.startDate)
+    console.log('Даныне клиента: ' + nsession.userAgent)
+    console.log('Возраст пользователя: : ' + nsession.userAge)
+}
+
+function filterContent()
+{
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].getElementsByTagName('h3')[0].innerText;
+
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
+
+/*
+* Всплывающее окно будет показано по таймауту
+* 
+* */
+// setTimeout(() =>
+//     alert("Нравится LifeSpot? " + '\n' +  "Подпишитесь на наш Instagram @lifespot999!" ),
+// 7000);
+
+
+
+
